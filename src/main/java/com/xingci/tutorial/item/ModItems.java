ce4559195f9309c538edfa0c1c208f466294274c
Memory2026 <2923361317@qq.com>
2026-04-18 17:32:47 +0800

    Initial commit

---
+++ src/main/java/com/xingci/tutorial/item/ModItems.java
@@ -0,0 +1,25 @@
+package com.xingci.tutorial.item;
+
+import com.xingci.tutorial.TutorialMod;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.EventBus;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS =
+            DeferredRegister.createItems(TutorialMod.MOD_ID);
+
+    public static final DeferredItem<Item> ICE_Ether =
+            ITEMS.registerSimpleItem("ice_ether");
+    public static final DeferredItem<Item> Raw_Ice_Ether =
+            ITEMS.registerSimpleItem("raw_ice_ether");
+
+    public static final DeferredItem<Item> CARDBOARD =
+            ITEMS.registerSimpleItem("material/cardboard");
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
